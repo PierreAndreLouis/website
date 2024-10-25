4cb7cfb7129aebf209988ad91d055893d0fe12df
LOUIS Pieddr-Andre <webdeveloper3030@gmail.com>
2024-10-25 11:25:11 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Ajouter_vehicule from "./components/ajouter_modifier/Ajouter_vehicule";
 import Modifier_vehicule from "./components/ajouter_modifier/Modifier_vehicule";
 import Navigation_bar from "./components/home/Navigation_bar";
 import Historique_voiture from "./components/historique/Historique_voiture";
-import Paiement from "./components/paiement/paiement";
+// import Paiement from "./components/paiement/paiement";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,7 +26,7 @@ function App() {
         <Route  path="/ajouter_vehicule" element={<Ajouter_vehicule />} />
         <Route  path="/modifier_vehicule" element={<Modifier_vehicule />} />
         <Route  path="/historique" element={<Historique_voiture />} />
-        <Route  path="/paiement" element={<Paiement />} />
+        {/* <Route  path="/paiement" element={<Paiement />} /> */}
       </Routes>
 
       {/* <Navigation_bar /> */}
