5954bee8411d400a3dfec65becebfdce14833f81
LOUIS Pieddr-Andre <webdeveloper3030@gmail.com>
2024-12-04 17:19:37 -0500

    correction dans la liste notactive purple color

--- src/components/home/Liste.jsx
+++ src/components/home/Liste.jsx
@@ -190,7 +190,9 @@ function Liste() {
             active_bg_color = "bg-purple-200/50 dark:bg-purple-600/50";
             vitess_img = "/img/home_icon/payer.png";
             imgClass = "w-14 sm:w-16 md:w-24";
-          } else if (speed < 1) {
+          }
+
+          if (speed < 1) {
             main_text_color = "text-red-900 dark:text-red-300";
             statut = "En arrêt";
             lite_bg_color =
